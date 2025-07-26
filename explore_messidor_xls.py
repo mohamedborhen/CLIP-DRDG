9339fdef3c039d9634aa5a9e1d84d67eac34a288
mohamedborhen <mohamedborhen.khlifi@ensi-uma.tn>
2025-07-26 20:57:04 +0200

    Update explore_messidor_xls.py

--- explore_messidor_xls.py
+++ explore_messidor_xls.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # 🔁 Change ce chemin pour tester chaque fichier
-xls_path = r"C:\Users\ensi02\Desktop\dataset DR detection\datasets\messidor\base12.xls"
+xls_path = r"put the path to messidor's base12.xls here"
 
 # Lire le fichier
 df = pd.read_excel(xls_path)
